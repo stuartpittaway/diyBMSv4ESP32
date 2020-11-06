94d326c640059d6321491cee47093a982cc6586b
Stuart Pittaway <1201909+stuartpittaway@users.noreply.github.com>
2020-11-06 07:57:16 +0000

    Update buildscript_versioning.py
    Fixed issue where user compiles code without using GIT (downloaded ZIP file instead)

--- ESPController/buildscript_versioning.py
+++ ESPController/buildscript_versioning.py
@@ -2,10 +2,15 @@
 import datetime
 import subprocess
 import os
+from os import path
 
 Import("env")
 
-# Get the latest GIT version header/name
-gitversion = subprocess.check_output(['git','log','--pretty=format:%h_%ad','-1','--date=short']).decode('utf-8')
-#print(gitversion)
-env.Append(CPPDEFINES=('-D GIT_VERSION=\\\"'+gitversion+'\\\"'))
+if (path.exists('..'+os.path.sep+'.git')):
+    # Get the latest GIT version header/name
+    gitversion = subprocess.check_output(['git','log','--pretty=format:%h_%ad','-1','--date=short']).decode('utf-8')
+    #print(gitversion)
+    env.Append(CPPDEFINES=('-D GIT_VERSION=\\\"'+gitversion+'\\\"'))
+else:
+    env.Append(CPPDEFINES=('-D GIT_VERSION=\\\"'+"Compiled without GIT"+'\\\"'))
+
