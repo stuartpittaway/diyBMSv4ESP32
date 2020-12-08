b89daed8431ba13ae255546bc8b4041266426db3
Stuart Pittaway <1201909+stuartpittaway@users.noreply.github.com>
2020-12-08 14:32:31 +0000

    Rename compiled output HEX files to match module version/type

---
+++ ATTINYCellModule/buildscript.py
@@ -0,0 +1,12 @@
+Import("env")
+
+# access to global construction environment
+#print(env)
+# access to project construction environment
+#print(projenv)
+my_flags = env.ParseFlags(env['BUILD_FLAGS'])
+defines = {k: v for (k, v) in my_flags.get("CPPDEFINES")}
+#print(defines.get("DIYBMSMODULEVERSION"))
+
+env.Replace(PROGNAME="diybms_module_firmware_%s%s" % (defines.get("DIYBMSMODULEVERSION") , "_SWAPR19R20" if "SWAPR19R20" in defines else "" ))
+
