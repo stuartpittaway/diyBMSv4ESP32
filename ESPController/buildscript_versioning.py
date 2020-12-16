6daf1a61d21908afb882d7a9d1962f4ecb911a29
Stuart Pittaway <1201909+stuartpittaway@users.noreply.github.com>
2020-12-16 11:22:58 +0000

    Update github actions

--- ESPController/buildscript_versioning.py
+++ ESPController/buildscript_versioning.py
@@ -9,6 +9,13 @@ Import("env")
 env.Replace(PROGNAME="diybms_controller_firmware_%s_%s" %
             (env["PIOPLATFORM"], env["PIOENV"]))
 
+
+env.Replace(ESP8266_FS_IMAGE_NAME="diybms_controller_filesystemimage_%s_%s" %
+            (env["PIOPLATFORM"], env["PIOENV"]))
+
+env.Replace(ESP32_SPIFFS_IMAGE_NAME="diybms_controller_filesystemimage_%s_%s" %
+            (env["PIOPLATFORM"], env["PIOENV"]))
+
 git_sha=None
 
 AreWeInGitHubAction = True if "GITHUB_SHA" in env else False
