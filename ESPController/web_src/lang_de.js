94688b03ac4c9a151479b678800faa084570e1c0
Anhetes <68035727+Anhetes@users.noreply.github.com>
2021-05-17 20:00:19 +0200

    Update and rename lang_es.js to lang_de.js
    German translation

---
+++ ESPController/web_src/lang_de.js
@@ -0,0 +1,51 @@
+// This file is for German language translation
+// https://www.w3schools.com/tags/ref_language_codes.asp
+
+$("#home").text("Home");
+$("#modules").text("Module");
+$("#settings").text("Einstellungen");
+$("#rules").text("Regeln");
+$("#more").text("mehr");
+
+$("#integration").text("Einbindung");
+$("#currentmonitor").text("diyBMS Amperemeter");
+$("#storage").text("Speicher");
+$("#avrprogrammer").text("AVR-Programmierer");
+$("#about").text("Informationen");
+
+$("#modulesPage > h1").text("Modul Seiten");
+
+$("#globalConfig > h2").text("Globale Einstellungen");
+$("#globalConfig > p").text("Konfigurieren Sie alle Module so, dass sie die folgenden Parameter verwenden:");
+
+$("#mpBank").text("Gruppe");
+$("#mpModule").text("Module");
+$("#mpVoltage").text("Volt");
+$("#mpvmin").text("V. Minimum ");
+$("#mptint").text("Temp intern &deg;C");
+$("#mptext").text("Temp extern &deg;C");
+$("#mpbypass").text("Bypass %");
+$("#mpbpc").text("Falsche Paketanzahl");
+$("#mppktr").text("Erhaltene Pakete");
+$("#mpbal").text("Gleichen Sie den Energieverbrauch aus (mAh)");
+
+$("#error1").text("Der Controller hat Probleme mit der Kommunikation der Überwachungsmodule.");
+$("#error3").text("Der Controller ist so konfiguriert, dass mehr Module verwendet werden, als er unterstützen kann.");
+$("#error4").text("Warten auf die Antwort der Module");
+$("#error5").text("Das Modul hat NULL-Volt gelesen. Überprüfen Sie die Konfiguration");
+$("#error6").text("Der Controller hat nicht genügend Speicher.");
+$("#error7").text("Not-Stop");
+$("#iperror").text("Kommunikation mit dem Controller für Statusaktualisierungen nicht möglich.");
+$("#jslibrary").text("Die JavaScript-Bibliothek wurde nicht korrekt geladen. Bitte aktualisieren Sie die Seite.");
+$("#saveerror").text("Die Einstellungen konnte nicht gespeichert werden.");
+$("#savesuccess").text("Einstellungen gespeichert");
+
+$("#warning1").text("Warnung: Die Bypass-Spannung der Module unterscheidet sich von der globalen Einstellung");
+$("#warning2").text("Warnung: Die Bypass-Temperatur der Module unterscheidet sich von der globalen Einstellung");
+$("#warning3").text("Warnung: Module haben verschiedene Codeversionen, das könnte zu Instabilität führen");
+$("#warning4").text("Warnung: Module haben verschiedene Hardware oder Board-Versionen");
+$("#warning5").text("Warnung: Prtokollierung aktiviert, SD-Karte jedoch nicht installiert oder gefunden");
+$("#warning6").text("Einige Funktionen sind deaktiviert, während der AVR-Programmiermodus aktiviert ist");
+
+$("label[for='NTPServer']").text("NTP Server");
+$("label[for='NTPZoneHour']").text("Zeitzone (Stunden)");
