af34d5924514c6c2bdbd940e65f136d86d47739f
Stuart Pittaway <1201909+stuartpittaway@users.noreply.github.com>
2021-10-05 11:10:49 +0100

    Fix for 2 issues (#59)
    * Fix issue #48
    
    * Fix for issue #51

--- ESPController/web_src/pagecode.js
+++ ESPController/web_src/pagecode.js
@@ -259,17 +259,16 @@ function configureModule(button, cellid, attempts) {
 }
 
 function secondsToHms(seconds) {
-
     seconds = Number(seconds);
     var d = Math.floor(seconds / (3600 * 24));
     var h = Math.floor(seconds % (3600 * 24) / 3600);
     var m = Math.floor(seconds % 3600 / 60);
     var s = Math.floor(seconds % 60);
 
-    var dDisplay = d > 0 ? h + "d" : "";
+    var dDisplay = d > 0 ? d + "d" : "";
     var hDisplay = h > 0 ? h + "h" : "";
     var mDisplay = m > 0 ? m + "m" : "";
-    var sDisplay = d > 1 ? "" : (s > 0 ? s + "s" : "");
+    var sDisplay = h > 0 ? "" : (s > 0 ? s + "s" : "");
     return dDisplay + hDisplay + mDisplay + sDisplay;
 }
 
