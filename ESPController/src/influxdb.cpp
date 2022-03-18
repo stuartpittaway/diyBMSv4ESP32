55f684aad2b2d4691856221bcd6c19f838e1583f
Stuart Pittaway <1201909+stuartpittaway@users.noreply.github.com>
2022-03-18 09:44:35 +0000

    Remove space between "cells,cell" + add error logging

--- ESPController/src/influxdb.cpp
+++ ESPController/src/influxdb.cpp
@@ -8,7 +8,7 @@
 */
 
 #define USE_ESP_IDF_LOG 1
-static constexpr const char * const TAG = "diybms-influxdb";
+static constexpr const char *const TAG = "diybms-influxdb";
 
 #include "influxdb.h"
 #include "string_utils.h"
@@ -21,11 +21,12 @@ static constexpr const char * const TAG = "diybms-influxdb";
 /// @param hex_len size of hex encoded string (automatically sized).
 ///
 /// @return hex encoded string.
-template <typename I> std::string to_hex(I w, size_t hex_len = sizeof(I) << 1)
+template <typename I>
+std::string to_hex(I w, size_t hex_len = sizeof(I) << 1)
 {
-    static const char* digits = "0123456789ABCDEF";
-    std::string rc(hex_len,'0');
-    for (size_t i = 0, j = (hex_len - 1) * 4 ; i < hex_len; ++i , j -= 4)
+    static const char *digits = "0123456789ABCDEF";
+    std::string rc(hex_len, '0');
+    for (size_t i = 0, j = (hex_len - 1) * 4; i < hex_len; ++i, j -= 4)
     {
         rc[i] = digits[(w >> j) & 0x0F];
     }
@@ -41,39 +42,39 @@ template <typename I> std::string to_hex(I w, size_t hex_len = sizeof(I) << 1)
 /// RFC: https://www.ietf.org/rfc/rfc1738.txt
 static inline std::string url_encode(const std::string source)
 {
-  const std::string reserved_characters = "?#/:;+@&=";
-  const std::string illegal_characters = "%<>{}|\\\"^`!*'()$,[]";
-  std::string encoded = "";
+    const std::string reserved_characters = "?#/:;+@&=";
+    const std::string illegal_characters = "%<>{}|\\\"^`!*'()$,[]";
+    std::string encoded = "";
 
-  // reserve the size of the source string plus 25%, this space will be
-  // reclaimed if the final string length is shorter.
-  encoded.reserve(source.length() + (source.length() / 4));
+    // reserve the size of the source string plus 25%, this space will be
+    // reclaimed if the final string length is shorter.
+    encoded.reserve(source.length() + (source.length() / 4));
 
-  // process the source string character by character checking for any that
-  // are outside the ASCII printable character range, in the reserve character
-  // list or illegal character list. For accepted characters it will be added
-  // to the encoded string directly, any that require encoding will be hex
-  // encoded before being added to the encoded string.
-  for (auto ch : source)
-  {
-    if (ch <= 0x20 || ch >= 0x7F ||
-        reserved_characters.find(ch) != std::string::npos ||
-        illegal_characters.find(ch) != std::string::npos)
+    // process the source string character by character checking for any that
+    // are outside the ASCII printable character range, in the reserve character
+    // list or illegal character list. For accepted characters it will be added
+    // to the encoded string directly, any that require encoding will be hex
+    // encoded before being added to the encoded string.
+    for (auto ch : source)
     {
-      // if it is outside the printable ASCII character *OR* is in either the
-      // reserved or illegal characters we need to encode it as %HH.
-      // NOTE: space will be encoded as "%20" and not as "+", either is an
-      // acceptable option per the RFC.
-      encoded.append("%").append(to_hex(ch));
+        if (ch <= 0x20 || ch >= 0x7F ||
+            reserved_characters.find(ch) != std::string::npos ||
+            illegal_characters.find(ch) != std::string::npos)
+        {
+            // if it is outside the printable ASCII character *OR* is in either the
+            // reserved or illegal characters we need to encode it as %HH.
+            // NOTE: space will be encoded as "%20" and not as "+", either is an
+            // acceptable option per the RFC.
+            encoded.append("%").append(to_hex(ch));
+        }
+        else
+        {
+            encoded += ch;
+        }
     }
-    else
-    {
-      encoded += ch;
-    }
-  }
-  // shrink the buffer to the actual length
-  encoded.shrink_to_fit();
-  return encoded;
+    // shrink the buffer to the actual length
+    encoded.shrink_to_fit();
+    return encoded;
 }
 
 /// HTTP Client event handler
@@ -83,29 +84,29 @@ static inline std::string url_encode(const std::string source)
 /// @return Always returns ESP_OK.
 static esp_err_t http_event_handler(esp_http_client_event_t *evt)
 {
-    switch(evt->event_id)
+    switch (evt->event_id)
     {
-        case HTTP_EVENT_ERROR:
-            ESP_LOGE(TAG, "HTTP Client Error encountered");
-            break;
-        case HTTP_EVENT_ON_CONNECTED:
-            ESP_LOGI(TAG, "HTTP Client Connected!");
-            break;
-        case HTTP_EVENT_HEADERS_SENT:
-            ESP_LOGV(TAG, "HTTP Client sent all request headers");
-            break;
-        case HTTP_EVENT_ON_HEADER:
-            ESP_LOGV(TAG, "Header: key=%s, value=%s", evt->header_key, evt->header_value);
-            break;
-        case HTTP_EVENT_ON_DATA:
-            ESP_LOGV(TAG, "HTTP Client data recevied: len=%d", evt->data_len);
-            break;
-        case HTTP_EVENT_ON_FINISH:
-            ESP_LOGI(TAG, "HTTP Client finished");
-            break;
-         case HTTP_EVENT_DISCONNECTED:
-            ESP_LOGI(TAG, "HTTP Client Disconnected!");
-            break;
+    case HTTP_EVENT_ERROR:
+        ESP_LOGE(TAG, "HTTP Client Error encountered");
+        break;
+    case HTTP_EVENT_ON_CONNECTED:
+        ESP_LOGI(TAG, "HTTP Client Connected!");
+        break;
+    case HTTP_EVENT_HEADERS_SENT:
+        ESP_LOGV(TAG, "HTTP Client sent all request headers");
+        break;
+    case HTTP_EVENT_ON_HEADER:
+        ESP_LOGV(TAG, "Header: key=%s, value=%s", evt->header_key, evt->header_value);
+        break;
+    case HTTP_EVENT_ON_DATA:
+        ESP_LOGV(TAG, "HTTP Client data recevied: len=%d", evt->data_len);
+        break;
+    case HTTP_EVENT_ON_FINISH:
+        ESP_LOGI(TAG, "HTTP Client finished");
+        break;
+    case HTTP_EVENT_DISCONNECTED:
+        ESP_LOGI(TAG, "HTTP Client Disconnected!");
+        break;
     }
     return ESP_OK;
 }
@@ -142,12 +143,16 @@ void influx_task_action()
             std::string module_bypass = cmi[moduleIndex].inBypass ? "true" : "false";
             std::string module_voltage = float_to_string(cmi[moduleIndex].voltagemV / 1000.0f);
 
-            ESP_LOGV(TAG, "Index:%d, bank:%d, module:%d, id:%s, voltage:%s, int-temp:%d, ext-temp:%s, bypass:%s",
-                moduleIndex, bank, module_in_bank, module_id.c_str(), module_voltage.c_str(),
-                module_internal_temp.c_str(), module_external_temp.c_str(), module_bypass.c_str());
+            ESP_LOGV(TAG, "Index:%d, bank:%d, module:%d, id:%s, voltage:%s, int-temp:%s, ext-temp:%s, bypass:%s",
+                     moduleIndex, bank, module_in_bank,
+                     module_id.c_str(),
+                     module_voltage.c_str(),
+                     module_internal_temp.c_str(),
+                     module_external_temp.c_str(),
+                     module_bypass.c_str());
 
-            //Data in LINE PROTOCOL format https://docs.influxdata.com/influxdb/v2.0/reference/syntax/line-protocol/
-            module_data.append("cells, cell=").append(module_id);
+            // Data in LINE PROTOCOL format https://docs.influxdata.com/influxdb/v2.0/reference/syntax/line-protocol/
+            module_data.append("cells,cell=").append(module_id);
             module_data.append(" v=").append(module_voltage);
             module_data.append(",i=").append(module_internal_temp).append("i");
             module_data.append(",e=").append(module_external_temp).append("i");
@@ -171,12 +176,13 @@ void influx_task_action()
         return;
     }
 
-    authtoken.reserve(128);
+    authtoken.reserve(128);  
     authtoken.append("Token ").append(mysettings.influxdb_apitoken);
     authtoken.shrink_to_fit();
 
-    url.reserve(348);
+    url.reserve(256);
     url.append(mysettings.influxdb_serverurl);
+    //influxdb_orgid seems to be over sized in the configuration, these are typically like this... "5852aba5c418c9e3"
     url.append("?org=").append(url_encode(mysettings.influxdb_orgid));
     url.append("&bucket=").append(url_encode(mysettings.influxdb_databasebucket));
     url.shrink_to_fit();
@@ -199,8 +205,18 @@ void influx_task_action()
         esp_http_client_set_post_field(http_client, module_data.c_str(), module_data.length()));
 
     // Process the http request.
-    ESP_ERROR_CHECK_WITHOUT_ABORT(esp_http_client_perform(http_client));
- 
+    esp_err_t err = esp_http_client_perform(http_client);
+
+    if (err == ESP_OK)
+    {
+        ESP_LOGI(TAG, "Status = %d, content_length = %d",
+                 esp_http_client_get_status_code(http_client),
+                 esp_http_client_get_content_length(http_client));
+    }
+    else
+    {
+        ESP_LOGE(TAG, "Error perform http request %s", esp_err_to_name(err));
+    }
     // Cleanup the http client.
     ESP_ERROR_CHECK_WITHOUT_ABORT(esp_http_client_cleanup(http_client));
 }
