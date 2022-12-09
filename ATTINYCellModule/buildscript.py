ea5ac8433996135be48beed6a631e79a5e392370
Stuart Pittaway <1201909+stuartpittaway@users.noreply.github.com>
2022-12-09 14:58:30 +0000

    Update buildscript.py

--- ATTINYCellModule/buildscript.py
+++ ATTINYCellModule/buildscript.py
@@ -2,19 +2,17 @@ Import("env")
 
 platform = env.PioPlatform()
 
+my_flags = env.ParseFlags(env['BUILD_FLAGS'])
+defines = {k: v for (k, v) in my_flags.get("CPPDEFINES")}
+#print(my_flags)
+#print(defines.get("DIYBMSMODULEVERSION"))
+
 if env.get("BOARD")=="attiny841":
     #print(env)
     #print(platform)
-
-    my_flags = env.ParseFlags(env['BUILD_FLAGS'])
-    #print(my_flags)
-    defines = {k: v for (k, v) in my_flags.get("CPPDEFINES")}
-    #print(defines.get("DIYBMSMODULEVERSION"))
-
     efuse=hex(int(env.GetProjectOption("board_fuses.efuse"), 2)).upper()[2:4]
     hfuse=hex(int(env.GetProjectOption("board_fuses.hfuse"), 2)).upper()[2:4]
     lfuse=hex(int(env.GetProjectOption("board_fuses.lfuse"), 2)).upper()[2:4]
-
     env.Replace(PROGNAME="module_fw_%s_%s_%s%s_e%s_h%s_l%s" % (env["PIOENV"],env.get("BOARD"), defines.get("DIYBMSMODULEVERSION"), "_SWAPR19R20" if "SWAPR19R20" in defines else "", efuse, hfuse, lfuse ))
 
 
