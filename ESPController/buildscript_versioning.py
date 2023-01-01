f535c87b6d784cbca3cee28d96ca5ae04c8f7443
Stuart Pittaway <1201909+stuartpittaway@users.noreply.github.com>
2023-01-01 13:22:20 +0000

    Cater for week zero in COMPILE_WEEK_NUMBER_BYTE

--- ESPController/buildscript_versioning.py
+++ ESPController/buildscript_versioning.py
@@ -16,17 +16,18 @@ env.Replace(ESP8266_FS_IMAGE_NAME="diybms_controller_filesystemimage_%s_%s" %
 env.Replace(ESP32_SPIFFS_IMAGE_NAME="diybms_controller_filesystemimage_%s_%s" %
             (env["PIOPLATFORM"], env["PIOENV"]))
 
-git_sha=None
+git_sha = None
 
 AreWeInGitHubAction = True if "GITHUB_SHA" in env else False
 
 if (AreWeInGitHubAction):
-    git_sha=env["GITHUB_SHA"]
+    git_sha = env["GITHUB_SHA"]
 else:
     if (path.exists('..'+os.path.sep+'.git')):
         # Get the latest GIT version header/name
         try:
-            git_sha = subprocess.check_output(['git','log','-1','--pretty=format:%H']).decode('utf-8')
+            git_sha = subprocess.check_output(
+                ['git', 'log', '-1', '--pretty=format:%H']).decode('utf-8')
         except:
             # Ignore any error, user may not have GIT installed
             git_sha = None
@@ -45,35 +46,34 @@ with open(os.path.join(include_dir, 'EmbeddedFiles_Defines.h'), 'w') as f:
     f.write("\n\n#ifndef EmbeddedFiles_Defines_H\n#define EmbeddedFiles_Defines_H\n\n")
 
     f.write("static const char GIT_VERSION[] = \"")
-    if (git_sha!=None):
+    if (git_sha != None):
         f.write(git_sha)
     else:
         f.write("LocalCompile")
     f.write("\";\n\n")
 
     f.write("static const char GIT_VERSION_SHORT[] = \"")
-    if (git_sha!=None):
+    if (git_sha != None):
         f.write(git_sha[-8:])
     else:
         f.write("LocalCompile")
     f.write("\";\n\n")
 
     f.write("static const uint16_t GIT_VERSION_B1 = 0x")
-    if (git_sha!=None):
+    if (git_sha != None):
         f.write(git_sha[32:36])
     else:
-        #Default for local compile
+        # Default for local compile
         f.write("FFFF")
     f.write(";\n\n")
 
     f.write("static const uint16_t GIT_VERSION_B2 = 0x")
-    if (git_sha!=None):
+    if (git_sha != None):
         f.write(git_sha[36:])
     else:
-        #Default for local compile
+        # Default for local compile
         f.write("FFFF")
     f.write(";\n\n")
-
 
     f.write("static const char COMPILE_DATE_TIME[] = \"")
     f.write(datetime.datetime.utcnow().isoformat()[:-3]+'Z')
@@ -88,7 +88,7 @@ with open(os.path.join(include_dir, 'EmbeddedFiles_Defines.h'), 'w') as f:
     f.write(";\n\n")
 
     f.write("static const uint8_t COMPILE_WEEK_NUMBER_BYTE = ")
-    f.write(datetime.datetime.utcnow().strftime("%W").lstrip("0"))
+    f.write(str(int(datetime.datetime.utcnow().strftime("%W"))))
     f.write(";\n\n")
 
     f.write("#endif")
