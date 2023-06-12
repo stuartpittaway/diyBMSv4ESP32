47ce5a65938398cd79558a516a6040a69ad2a933
Stuart Pittaway <1201909+stuartpittaway@users.noreply.github.com>
2023-06-12 12:49:15 +0100

    PYLON text to uint8 array

--- ESPController/src/pylon_canbus.cpp
+++ ESPController/src/pylon_canbus.cpp
@@ -223,7 +223,8 @@ void pylon_message_35c()
 void pylon_message_35e()
 {
   // Send 8 byte "magic string" PYLON (with 3 trailing spaces)
-  const char pylon[] = "\x50\x59\x4c\x4f\x4e\x20\x20\x20";
+  //const char pylon[] = "\x50\x59\x4c\x4f\x4e\x20\x20\x20";
+  uint8_t pylon[]={0x50,0x59,0x4c,0x4f,0x4e,0x20,0x20,0x20};
   send_canbus_message(0x35e, (uint8_t *)&pylon, sizeof(pylon) - 1);
 }
 
