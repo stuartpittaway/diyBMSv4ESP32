1227bb92acffc434a8b9d918d760cf62f8ece840
Stuart Pittaway <1201909+stuartpittaway@users.noreply.github.com>
2023-08-08 15:31:39 +0100

    Update buildscript.py

--- STM32All-In-One/buildscript.py
+++ STM32All-In-One/buildscript.py
@@ -2,17 +2,14 @@ Import("env")
 
 platform = env.PioPlatform()
 
-my_flags = env.ParseFlags(env['BUILD_FLAGS'])
-defines = {k: v for (k, v) in my_flags.get("CPPDEFINES")}
+#my_flags = env.ParseFlags(env['BUILD_FLAGS'])
+#defines = {k: v for (k, v) in my_flags.get("CPPDEFINES")}
 #print(my_flags)
 #print(defines.get("DIYBMSMODULEVERSION"))
 #print(str(env["BOARD_MCU"]).lower())
 
 
 if str(env["BOARD_MCU"]).lower()=="stm32f030k6t6":
-    efuse=0
-    hfuse=0
-    lfuse=0
     env.Replace(PROGNAME="module_fw_%s_%s" % (env["PIOENV"],env.get("BOARD")))
 
 
