420c67055a5bb749d16114c7f3dac3742b82c143
Stuart Pittaway <1201909+stuartpittaway@users.noreply.github.com>
2023-11-13 12:36:13 +0000

    Update EmbeddedFiles_Defines.h

--- STM32All-In-One/include/EmbeddedFiles_Defines.h
+++ STM32All-In-One/include/EmbeddedFiles_Defines.h
@@ -5,12 +5,12 @@
 #ifndef EmbeddedFiles_Defines_H
 #define EmbeddedFiles_Defines_H
 
-static const char GIT_VERSION[] = "a4f3024afd95548a3a8628db190a98564f7cd2d5";
+static const char GIT_VERSION[] = "256670999f7ae2237b8ed7a913f5b9631bb5f405";
 
-static const uint16_t GIT_VERSION_B1 = 0x4f7c;
+static const uint16_t GIT_VERSION_B1 = 0x1bb5;
 
-static const uint16_t GIT_VERSION_B2 = 0xd2d5;
+static const uint16_t GIT_VERSION_B2 = 0xf405;
 
-static const char COMPILE_DATE_TIME[] = "2023-07-04T15:47:51.443Z";
+static const char COMPILE_DATE_TIME[] = "2023-11-13T11:48:01.770Z";
 
 #endif
